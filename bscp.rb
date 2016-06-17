85b3fad0810e4322be5e17dc6cf2ccaa9e4c8cf4
Jason Kingsbury <jkingsbury@illumina.com>
2016-06-17 14:39:28 +0100

    initial bscp commit

---
+++ bscp.rb
@@ -0,0 +1,9 @@
+class Bscp < Formula
+  homepage "https://illumina.com"
+  url "https://bintray.com/basespace/BaseSpaceCLI-TGZ/download_file?file_path=bscp-osx-0.3.1.tar.gz"
+  sha256 "540eb0b0162d7fdf871d0057744c78182c563f71c8a4626865c98bf7eab41a5d"
+
+  def install
+  	bin.install "bscp"
+  end
+end
