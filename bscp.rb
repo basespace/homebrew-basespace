12be6c2729be961a706c86d21b95dad743e660d0
Jason Kingsbury <jkingsbury@illumina.com>
2016-06-22 16:54:24 +0100

    fix SHA256

--- bscp.rb
+++ bscp.rb
@@ -1,7 +1,7 @@
 class Bscp < Formula
   homepage "https://illumina.com"
   url "https://bintray.com/basespace/BaseSpaceCLI-TGZ/download_file?file_path=bscp-osx-0.3.2.tar.gz"
-  sha256 "540eb0b0162d7fdf871d0057744c78182c563f71c8a4626865c98bf7eab41a5d"
+  sha256 "ee0447996d0ae5e61d753a73d2a74d72bcb2cae4b96d0fcb6de1e94420af5522"
 
   def install
   	bin.install "bscp"
