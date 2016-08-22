ab5c913c1f7e0cc2f33f90e7c99b264e9f62df55
Jason Kingsbury <jkingsbury@illumina.com>
2016-08-22 18:53:03 +0100

    bscp v0.4.2

--- bscp.rb
+++ bscp.rb
@@ -1,7 +1,7 @@
 class Bscp < Formula
   homepage "https://illumina.com"
-  url "https://bintray.com/basespace/BaseSpaceCLI-TGZ/download_file?file_path=bscp-osx-0.4.1.tar.gz"
-  sha256 "3d912e5829161a5fe1df751653946f9856db5e9699651c0bc8911ec57c196734"
+  url "https://bintray.com/basespace/BaseSpaceCLI-TGZ/download_file?file_path=bscp-osx-0.4.2.tar.gz"
+  sha256 "9a8b4f4f347eb134060aec72f2509f0c54c855ef9b9268c88fb7b4dad0f33b94"
 
   def install
   	bin.install "bscp"
