516710fef6c6f0b2bd0441095d4d0f34743b8e72
Benjamin L. Moore <blmoore@users.noreply.github.com>
2019-04-09 10:24:28 +0100

    update to v0.10.8 release

--- bs-cli.rb
+++ bs-cli.rb
@@ -1,8 +1,8 @@
 class BsCli < Formula
   desc "BaseSpace Sequence Hub Command Line Interface"
   homepage "https://developer.basespace.illumina.com/docs/content/documentation/cli/cli-overview"
-  url "https://dl.bintray.com/basespace/BaseSpaceCLI-EarlyAccess-BIN/latest/0.10.7/amd64-osx/bs"
-  sha256 "9663f03c14daebdaaf480034016382be3e6afc3cac163116e63e788259aee973"
+  url "https://dl.bintray.com/basespace/BaseSpaceCLI-EarlyAccess-BIN/latest/0.10.8/amd64-osx/bs"
+  sha256 "61c5477758adf3cdef0a2d16a6b878490f8b235b8db1649790bfc4e619e0099c"
 
   def install
     bin.install "bs"
