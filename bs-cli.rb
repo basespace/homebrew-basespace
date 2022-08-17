3b8239f72ebaa228e34a50c39f04af8355f86e88
Benjamin L. Moore <blmoore@users.noreply.github.com>
2022-08-17 10:48:08 +0100

    update to 1.5.2

--- bs-cli.rb
+++ bs-cli.rb
@@ -1,8 +1,8 @@
 class BsCli < Formula
   desc "BaseSpace Sequence Hub Command Line Interface"
   homepage "https://developer.basespace.illumina.com/docs/content/documentation/cli/cli-overview"
-  url "https://launch.basespace.illumina.com/CLI/1.5.1/amd64-osx/bs"
-  sha256 "d402584247496ebd0f4718c450e9069089781b893e190bb34f81b775d691bd2e"
+  url "https://launch.basespace.illumina.com/CLI/1.5.2/amd64-osx/bs"
+  sha256 "baf0a6fcee2bb46b6148405e8bc983c0cc7ea7ebb95ca6d701fadaef9a558b7c"
 
   def install
     bin.install "bs"
